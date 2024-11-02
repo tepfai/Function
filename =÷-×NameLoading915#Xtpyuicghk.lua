b22e4827900de3ae58ec3db104899fe02de5e483
tepfai <hoalenguyen839@gmail.com>
2024-11-02 15:55:51 +0700

    Add files via upload

---
+++ =÷-×NameLoading915#Xtpyuicghk.lua
@@ -0,0 +1,3 @@
+local PlayerName = game.Players.LocalPlayer.DisplayName
+require(game.ReplicatedStorage.Notification).new("<Color=Purple>Welcome "..PlayerName.."<Color=/>"):Display()
+require(game.ReplicatedStorage.Notification).new("<Color=Yellow>Enjoy For Script<Color=/>"):Display()
